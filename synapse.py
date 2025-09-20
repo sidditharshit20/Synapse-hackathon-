b8f975cd618c660cdb5b4d5bc7666bf75c78c87c
sashwatkumarpathak <sashwat2008@gmail.com>
2025-09-20 15:16:44 +0530

    Testing API - 1

--- synapse.py
+++ synapse.py
@@ -6,20 +6,20 @@ from typing import Dict, List, Tuple, Optional
 
 import requests
 
-API_KEY = "http://indianrailapi.com/api/v2/livetrainstatus/apikey/<apikey>/trainnumber/<train_number>/date/<yyyymmdd>/"
-SRC = "NDLS"    # e.g. New Delhi
-DEST = "BCT"    # e.g. Mumbai CSTM
-DATE = "10-10-2025"
+API_KEY = "http://indianrailapi.com/api/v2/livetrainstatus/apikey/<apikey>/trainnumber/<train_number>/date/<yyyymmdd>/"   # replace with your key
+TRAIN_NO = "12727"
+DATE = "20250919"  # YYYYMMDD
 
-url = f"https://indianrailapi.com/api/v2/between/source/dest/journey/{SRC}/{DEST}/{DATE}"
-params = {
-    "api_key": API_KEY
-}
+url = f"http://indianrailapi.com/api/v2/livetrainstatus/apikey/{API_KEY}/trainnumber/{TRAIN_NO}/date/{DATE}/"
 
-resp = requests.get(url, params=params)
+resp = requests.get(url)
+
 if resp.status_code == 200:
     data = resp.json()
-    # Process data
-    print(data)
+    print("Train:", data.get("TrainName"))
+    print("Number:", data.get("TrainNumber"))
+    print("Status:", data.get("Position"))
+    print("Current Station:", data.get("CurrentStation"))
+    print("Delay (Arrival):", data.get("DelayInArrival"))
 else:
     print("Error:", resp.status_code, resp.text)
