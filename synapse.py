725e291713f7fdd404435aab4a58356eaca744b6
sashwatkumarpathak <sashwat2008@gmail.com>
2025-09-21 16:10:22 +0530

    Adding JSON's and our first API integration attempt

--- synapse.py
+++ synapse.py
@@ -4,22 +4,15 @@ import copy
 import threading
 from typing import Dict, List, Tuple, Optional
 
-import requests
+import http.client
 
-API_KEY = "http://indianrailapi.com/api/v2/livetrainstatus/apikey/<apikey>/trainnumber/<train_number>/date/<yyyymmdd>/"   # replace with your key
-TRAIN_NO = "12727"
-DATE = "20250919"  # YYYYMMDD
+conn = http.client.HTTPSConnection("railradar.in")
 
-url = f"http://indianrailapi.com/api/v2/livetrainstatus/apikey/{API_KEY}/trainnumber/{TRAIN_NO}/date/{DATE}/"
+headers = { 'x-api-key': "rr_live_EXPo2kM5HHtFeQiGlZm6w4FhBJHmlaYj" }
 
-resp = requests.get(url)
+conn.request("GET", "/api/v1/trains/live-map", headers=headers)
 
-if resp.status_code == 200:
-    data = resp.json()
-    print("Train:", data.get("TrainName"))
-    print("Number:", data.get("TrainNumber"))
-    print("Status:", data.get("Position"))
-    print("Current Station:", data.get("CurrentStation"))
-    print("Delay (Arrival):", data.get("DelayInArrival"))
-else:
-    print("Error:", resp.status_code, resp.text)
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
